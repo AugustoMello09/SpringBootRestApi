326e3846eec3f25ce77ffb336b58c41a9478fb14
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-01 09:53:56 -0300

    projeto criado

---
+++ src/main/java/io/github/AugustoMello09/EntregaLogApplication.java
@@ -0,0 +1,13 @@
+package io.github.AugustoMello09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/AugustoMello09/EntregaLogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.AugustoMello09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
