63d95991c0c7c3e2c24caf53690fd32b84d883ac
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-02 10:49:21 -0300

    Usando jakarta Persistence JPA

--- src/main/java/io/github/AugustoMello09/model/Cliente.java
+++ src/main/java/io/github/AugustoMello09/model/Cliente.java
@@ -1,7 +1,19 @@
 package io.github.AugustoMello09.model;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
 	private String telefone;
@@ -29,6 +41,23 @@ public class Cliente {
 	}
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
