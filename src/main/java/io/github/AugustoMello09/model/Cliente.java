6527a553875dbad46449fc2986742a67406090fc
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-06 08:32:09 -0300

    Validando com Bean Validation

--- src/main/java/io/github/AugustoMello09/model/Cliente.java
+++ src/main/java/io/github/AugustoMello09/model/Cliente.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente {
@@ -14,8 +17,17 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 60)
 	private String telefone;
 	
 	public Long getId() {
