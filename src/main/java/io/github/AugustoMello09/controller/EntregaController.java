e5cb2faec35adff842ac2ce28b069c055db8a784
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-07 10:34:56 -0300

    EntregaDTO

--- src/main/java/io/github/AugustoMello09/controller/EntregaController.java
+++ src/main/java/io/github/AugustoMello09/controller/EntregaController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.AugustoMello09.dto.DestinatarioDTO;
+import io.github.AugustoMello09.dto.EntregaDTO;
 import io.github.AugustoMello09.model.Entrega;
 import io.github.AugustoMello09.repository.EntregaRepository;
 import io.github.AugustoMello09.service.SolicitacaoEntregaService;
@@ -22,28 +24,42 @@ import io.github.AugustoMello09.service.SolicitacaoEntregaService;
 @RestController
 @RequestMapping("/entregas")
 public class EntregaController {
-	
+
 	@Autowired
 	private SolicitacaoEntregaService service;
-	
+
 	@Autowired
 	private EntregaRepository entregaRepository;
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
 		return service.solicitar(entrega);
 	}
-	
+
 	@GetMapping
 	public List<Entrega> listar() {
 		return entregaRepository.findAll();
 	}
-	
+
 	@GetMapping("/{entregaId}")
-	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
-		return entregaRepository.findById(entregaId)
-				.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<EntregaDTO> buscar(@PathVariable Long entregaId) {
+		return entregaRepository.findById(entregaId).map(obj -> {
+			EntregaDTO entregaDto = new EntregaDTO();
+			entregaDto.setId(obj.getId());
+			entregaDto.setNomeCliente(obj.getCliente().getNome());
+			entregaDto.setDestinatario(new DestinatarioDTO());
+			entregaDto.getDestinatario().setNome(obj.getDestinatario().getNome());
+			entregaDto.getDestinatario().setLogradouro(obj.getDestinatario().getLogradouro());
+			entregaDto.getDestinatario().setNumero(obj.getDestinatario().getNumero());
+			entregaDto.getDestinatario().setComplemento(obj.getDestinatario().getComplemento());
+			entregaDto.getDestinatario().setBairro(obj.getDestinatario().getBairro());
+			entregaDto.setTaxa(obj.getTaxa());
+			entregaDto.setStatus(obj.getStatus());
+			entregaDto.setDataPedido(obj.getDataPedido());
+			entregaDto.setDataFinalizacao(obj.getDataFinalizacao());
+
+			return ResponseEntity.ok(entregaDto);
+		}).orElse(ResponseEntity.notFound().build());
 	}
 }
---
+++ src/main/java/io/github/AugustoMello09/dto/EntregaDTO.java
@@ -0,0 +1,62 @@
+package io.github.AugustoMello09.dto;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import io.github.AugustoMello09.model.enums.StatusEntrega;
+
+public class EntregaDTO {
+	
+	private Long id;
+	private String nomeCliente;
+	private DestinatarioDTO destinatario;
+	private BigDecimal taxa;
+	private StatusEntrega status;
+	private OffsetDateTime dataPedido;
+	private OffsetDateTime dataFinalizacao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public DestinatarioDTO getDestinatario() {
+		return destinatario;
+	}
+	public void setDestinatario(DestinatarioDTO destinatario) {
+		this.destinatario = destinatario;
+	}
+	public BigDecimal getTaxa() {
+		return taxa;
+	}
+	public void setTaxa(BigDecimal taxa) {
+		this.taxa = taxa;
+	}
+	public StatusEntrega getStatus() {
+		return status;
+	}
+	public void setStatus(StatusEntrega status) {
+		this.status = status;
+	}
+	public OffsetDateTime getDataPedido() {
+		return dataPedido;
+	}
+	public void setDataPedido(OffsetDateTime dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+	public OffsetDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+
+
+}
